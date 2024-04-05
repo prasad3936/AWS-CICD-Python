0eb0cc809af3cc633036992004a02d82ef7d5c86
Prasad C Zungare Patil <63768420+prasad3936@users.noreply.github.com>
2024-04-05 15:39:10 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -6,4 +6,4 @@ docker pull praszp246/monitor-flask-app:latest
 
 # Run the Docker image as a container
 
-docker run -d -p 5000:5000 praszp246/monitor-flask-app:latest .
+docker run -d -p 5000:5000 praszp246/monitor-flask-app:latest 
