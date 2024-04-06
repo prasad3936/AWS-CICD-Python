7cf83dfa68421fdb27fd17515ad09c18e7642dbe
Prasad C Zungare Patil <63768420+prasad3936@users.noreply.github.com>
2024-04-06 10:57:21 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -2,6 +2,6 @@
 set -e
 
 # Stop the running container (if any)
-container_id=$(sudo docker ps -q | head -n 1)
-sudo docker rm -f "$container_id"
+#container_id=$(sudo docker ps -q | head -n 1)
+#sudo docker rm -f "$container_id"
 
